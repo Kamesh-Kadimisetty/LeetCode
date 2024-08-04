051234a2eb80d873bcf64d836d1428cafec66b39
KameshK2005 <156226215+KameshK2005@users.noreply.github.com>
2024-08-04 11:28:43 +0530

    Time: 84 ms (75.73%) | Memory: 13.6 MB (75.95%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        int N=n*(n+1)/2;
+        int arr[N],k=0;
+        for(int i=0;i<n;i++){
+                int s=0;
+            for(int j=i;j<n;j++){
+                s+=nums[j];
+                arr[k++]=s;
+            }
+        }
+        sort(arr,arr+N);
+        int sum=0;
+        const int mod=1e9+7;
+        for(int i=left-1;i<right;i++){
+            sum=(sum+arr[i])%mod;
+        }
+        return sum;
+    }
+};
