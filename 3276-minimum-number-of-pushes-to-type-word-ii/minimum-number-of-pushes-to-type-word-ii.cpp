94c86c8ec99e16aa902f940eac462f98dbf31522
KameshK2005 <156226215+KameshK2005@users.noreply.github.com>
2024-08-06 21:27:32 +0530

    Time: 61 ms (95.56%) | Memory: 24.9 MB (62.15%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int minpushes=0,key=0;
+        int minpushes=0;
         int hash[26]={0};
         for(int i=0;i<word.size();i++){
             hash[word[i]-'a']++;
@@ -9,25 +9,14 @@ public:
         sort(hash,hash+26);
         reverse(hash,hash+26);
         for(int i=0;i<26;i++){
-             cout<<hash[i]<<" ";
-        }
-        for(int i=0;i<26;i++){
-            if(i<8){
-            key=1;
-            minpushes+=(hash[i]*key);
-            }
-            else if(i>=8 && i<16){
-            key=2;
-            minpushes+=(hash[i]*key);
-            }
-            else if(i>=16 && i<24){
-            key=3;
-            minpushes+=(hash[i]*key);
-            }
-            else{
-            key=4;
-            minpushes+=(hash[i]*key); 
-            }
+            if(i<8)
+            minpushes+=(hash[i]*1);
+            else if(i>=8 && i<16)
+            minpushes+=(hash[i]*2);
+            else if(i>=16 && i<24)
+            minpushes+=(hash[i]*3);
+            else
+            minpushes+=(hash[i]*4); 
         }
         return minpushes;
     }
