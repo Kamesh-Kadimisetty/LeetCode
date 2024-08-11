742984714a490f9d66c2f62be5159035d343a756
KameshK2005 <156226215+KameshK2005@users.noreply.github.com>
2024-08-11 07:39:05 +0530

    Time: 78 ms (84.85%) | Memory: 96.3 MB (16.61%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,26 +1,15 @@
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int profit=0,buy=prices[0];
-//         for(int i=0;i<prices.size();i++){
-//             if(prices[i]<buy){
-//                 buy=prices[i];
-//             }
-//             else if(prices[i]-buy>profit){
-//                 profit=prices[i]-buy;
-//             }
-//         }
-//         return profit;
-//     }
-// };
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int mini=prices[0],profit=0;
+        //Kadane's Algorithm
+        int profit=0,buy=prices[0];
         for(int i=0;i<prices.size();i++){
-            int cost=prices[i]-mini;
-            profit=max(profit,cost);
-            mini=min(mini,prices[i]);
+            if(prices[i]<buy){
+                buy=prices[i];
+            }
+            else if(prices[i]-buy>profit){
+                profit=prices[i]-buy;
+            }
         }
         return profit;
     }
