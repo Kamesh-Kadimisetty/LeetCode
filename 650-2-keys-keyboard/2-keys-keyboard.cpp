ff8e5c3a2f5d200c62b6af98c1056185419f8c0b
KameshK2005 <156226215+KameshK2005@users.noreply.github.com>
2024-08-19 10:58:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (80.37%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     int minSteps(int n) {
         if(n==1) return 0;
-        vector<int>prime;
-        while(n%2==0){
-            prime.push_back(2);
-            n/=2;
-        }
-        for(int i=3;i*i<=n;i+=2){
-            while(n%i==0){
-                prime.push_back(i);
-                n/=i;
+        int factor=2;
+        int steps=0;
+        while(n>1){
+            while(n%factor==0){
+                steps+=factor;
+                n/=factor;
             }
+            factor++;
         }
-        if(n>2){
-            prime.push_back(n);
-        }
-        return accumulate(prime.begin(),prime.end(),0);
+        return steps;
     }
 };
