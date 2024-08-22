02f63ad68f89417f6d84ea58c0493095a12fa341
KameshK2005 <156226215+KameshK2005@users.noreply.github.com>
2024-08-22 23:57:43 +0530

    Time: 20 ms (21.03%) | Memory: 35.2 MB (17.39%) - LeetSync

--- 3533-snake-in-matrix/snake-in-matrix.cpp
+++ 3533-snake-in-matrix/snake-in-matrix.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
     int finalPositionOfSnake(int n, vector<string>& commands) {
-        vector<vector<int>> ans(n,vector<int>(n,0));
-        int k=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ans[i][j]=k;
-                k++;
-            }
+        int c=0;
+        for(auto it :commands){
+            if(it=="DOWN") c+=n;
+            else if(it=="UP") c-=n;
+            else if(it=="LEFT") c-=1;
+            else if(it=="RIGHT") c+=1; 
         }
-        int a=0,b=0;
-        for(int i=0;i<commands.size();i++){
-            if(commands[i]=="DOWN"){
-                a++;
-            }
-            else if(commands[i]=="UP"){
-                a--;
-            }
-            else if(commands[i]=="RIGHT"){
-                b++;
-            }
-            else{
-                b--;
-            }
-        }
-        return ans[a][b];
+        return c;
     }
 };
