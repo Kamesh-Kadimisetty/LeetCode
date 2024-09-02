61771108615f1c66b0cbd843f85f4ceb08387ff0
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-09-02 18:17:39 +0530

    Time: 105 ms (20.21%) | Memory: 77.2 MB (8.71%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        int n=chalk.size(),i,res=-1;
+        int n=chalk.size();
         long long chalksum=accumulate(chalk.begin(),chalk.end(),0LL);
         k=k%chalksum;
         for(int i=0;i<n;i++){
             if(chalk[i]>k){
-                res=i;
-                break;
+                return i;
             }
-            else{
-                k-=chalk[i];
-            }
+            k-=chalk[i];
         }
-        return res;
+        return -1;
     }
 };
