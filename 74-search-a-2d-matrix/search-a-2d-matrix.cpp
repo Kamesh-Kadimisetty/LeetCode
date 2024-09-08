1e44bd998e15c0e12ac968c7a9169db57541a5c2
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-09-08 22:41:28 +0530

    Time: 3 ms (65.44%) | Memory: 12.2 MB (77.07%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+    int binary_search(vector<int>& matrix,int target){
+        int low=0,high=matrix.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(matrix[mid]==target) return mid;
+            else if(matrix[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
+    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
+            int ind=binary_search(matrix[i],target);
+            if(ind!=-1) return true;
         }
         return false;
     }
