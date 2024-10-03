99540e6b625ebc9e6d94c7225fe6b948e07adc37
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-10-03 23:59:04 +0530

    Time: 157 ms (74.71%) | Memory: 116.9 MB (82.86%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* prev=NULL;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            ListNode* front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL && head->next==NULL) return head;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* newhead=reverse(slow->next);
+        fast=head;
+        while(newhead!=NULL){
+            if(fast->val!=newhead->val){
+                return false;
+            }
+            newhead=newhead->next;
+            fast=fast->next;
+        }
+
+        return true;
+    }
+};
