4850f858be162c60afdc3a7f92a6762b150e875b
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-10-15 07:53:04 +0530

    Time: 32 ms (61.26%) | Memory: 15.7 MB (14.11%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        long long count=0,ans=0;
-        for(int i=s.size()-1;i>=0;i--){
-            if(s[i]=='1'){
-                ans+=count;
-            }
-            else{
-                count++;
+        long long ans=0;
+        for(int i=0,j=0;i<s.size();i++){
+            if(s[i]=='0'){
+                ans+=(i-j);
+                j++;
             }
         }
         return ans;
