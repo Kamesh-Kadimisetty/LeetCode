677e31acf0e6706920f49326191802813b1d0c8a
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-10-17 00:30:33 +0530

    Time: 5 ms (9.72%) | Memory: 10.3 MB (41.80%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,23 +11,15 @@
  */
 class Solution {
 public:
+    void preorder(TreeNode* root,vector<int>& ans){
+        if(root==NULL) return ;
+        preorder(root->left,ans);
+        preorder(root->right,ans);
+        ans.push_back(root->val);
+    }
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>result;
-        stack<TreeNode*>transversalstack;
-        TreeNode* currentnode=root;
-        while(currentnode!=nullptr||!transversalstack.empty()){
-            if(currentnode!=nullptr){
-                result.push_back(currentnode->val);
-                transversalstack.push(currentnode);
-                currentnode=currentnode->right;
-            }
-            else{
-                currentnode=transversalstack.top();
-                transversalstack.pop();
-                currentnode=currentnode->left;
-            }
-        }
-        reverse(result.begin(),result.end());
-        return result;
+        vector<int>ans;
+        preorder(root,ans);
+        return ans;
     }
 };
