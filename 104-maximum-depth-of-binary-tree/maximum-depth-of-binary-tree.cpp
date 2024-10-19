c4bf75f96cdbb0188545f023af3c9e41d6bff9e9
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-10-19 21:53:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.09%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,6 +12,7 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        return root == nullptr ? 0 : 1 + max ( maxDepth(root->left) , maxDepth(root->right) );
+        if(root==NULL) return 0;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
     }
 };
