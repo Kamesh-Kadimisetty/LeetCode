20874d5b692548bd147f50ab6e30c82ee703f07b
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-10-24 19:29:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (55.24%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -4,17 +4,19 @@ public:
         vector<vector<int>>res;
         if(root==nullptr) return res;
         queue<TreeNode*>st;
+        bool lefttoright=true;
         st.push(root);
         while(!st.empty()){
             long long sum=0;
             int n=st.size();
-            vector<int>ans;
+            vector<int>ans(n);
             for(int i=0;i<n;i++){
                 TreeNode* it=st.front();
                 st.pop();
+                int index=lefttoright?i:n-i-1;
+                ans[i]=it->val;
                 if(it->right) st.push(it->right);
                 if(it->left)  st.push(it->left);
-                ans.push_back(it->val);
             }
             res.push_back(ans);
         }
