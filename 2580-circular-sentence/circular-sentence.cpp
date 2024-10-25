abdc37b66e0b0fc29cebde265010cd5c37ec35a3
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-10-25 18:05:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (58.45%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        vector<string>res;
-        int ind=0,count=0;
-        sentence.append(" ");
-        for(int i=0;i<sentence.size();i++){
-            if(sentence[i]==' '){
-                res.push_back(sentence.substr(ind,count));
-                ind=i+1;
-                count=0;
-            }
-            else{
-                count++;
-            }
-        }
-        int n=res.size();
-        if(n==1){
-            if(res[0][0]!=res[0][res[0].size()-1]){
-                return false;
-            }
+        int n=sentence.size();
+        if(sentence[0]!=sentence[n-1]){
+            return false;
         }
         for(int i=0;i<n-1;i++){
-            if(res[i][res[i].size()-1]!=res[i+1][0]){
-                return false;
+            if(sentence[i]==' '){
+                if(sentence[i-1]!=sentence[i+1]){
+                    return false;
+                }
             }
-        }
-        if(res[0][0]!=res[n-1][res[n-1].size()-1]){
-            return false;
         }
         return true;
     }
