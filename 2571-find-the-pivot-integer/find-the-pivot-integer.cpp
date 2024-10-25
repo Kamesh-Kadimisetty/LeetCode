cf3244cd225b55582e7ebb7b2cd6c3e537a7d5d8
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-10-25 20:05:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (17.06%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int arr[n];
-        int arr1[n];
-        arr[0]=1;
-        arr1[n-1]=n;
+        if(n==1) return 1;
         for(int i=1;i<n;i++){
-            arr[i]=arr[i-1]+i+1;
-        }
-        for(int i=n-2;i>=0;i--){
-            arr1[i]=arr1[i+1]+i+1;
-        }
-        for(int i=0;i<n;i++){
-            if(arr[i]==arr1[i]){
-                return i+1;
+            if((i*(i+1)/2==((i+n)*(n-i+1))/2)){
+                return i;
             }
         }
         return -1;
