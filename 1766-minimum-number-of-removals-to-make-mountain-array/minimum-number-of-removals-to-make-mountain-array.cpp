6a7425db91d946e729b5a7ae72ec8b34fc9d1c00
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-10-30 22:13:21 +0530

    Time: 1 ms (95.45%) | Memory: 15.6 MB (18.29%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -13,11 +13,49 @@ public:
         }
         return dp;
     }
+     vector<int> LIS(vector<int>nums)
+    {
+        int n=nums.size();
+        vector<int>dp(n,1);
+        vector<int>helper;
+        helper.push_back(nums[0]);
+        int ind=0;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>helper[ind]) {
+                ind++;
+                helper.push_back(nums[i]);
+            }
+            else {
+                putproper(nums[i],helper,ind);
+            }
+            dp[i]=ind+1;
+        }
+        return dp;
+    }
+    void putproper(int val,vector<int>&arr,int lastind) 
+    {
+        int start=0;
+        int end=lastind;
+        int mid;
+        while(start<end)
+        {
+            mid=start+(end-start)/2;
+            if(arr[mid]==val)
+                return ;
+            else if(arr[mid]>val)
+                end=mid;
+            else
+                start=mid+1;
+        }
+        arr[start]=val;
+        
+    }
     int minimumMountainRemovals(vector<int>& nums) {
         int n=nums.size();
-        vector<int>increasing=lis(nums);  //LIS
+        vector<int>increasing=LIS(nums);  //LIS
         reverse(nums.begin(),nums.end());
-        vector<int>decreasing=lis(nums);  //LDS
+        vector<int>decreasing=LIS(nums);  //LDS
         reverse(decreasing.begin(),decreasing.end());
         int ans=INT_MAX;
         for(int i=0;i<n;i++){
