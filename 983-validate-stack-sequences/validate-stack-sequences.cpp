21b81c2adb1761b18d7b808f5c50d41eb334e2b6
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-11-04 07:21:52 +0530

    Time: 2 ms (17.87%) | Memory: 18.1 MB (87.66%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int>st;
+        int j=0;
+        for(int i=0;i<pushed.size();i++){
+            st.push(pushed[i]);
+            while(!st.empty() && popped[j]==st.top()){
+                st.pop();
+                j++;
+            }
+        }
+        cout<<j;
+        return j==popped.size();
+    }
+};
