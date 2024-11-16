184b1ee94e8c1aff8e123a2694b76f8ecc132a92
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-11-16 16:32:26 +0530

    Time: 3 ms (46.25%) | Memory: 32.9 MB (91.28%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int powerarr(vector<int>nums){
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=nums[i-1]+1){
-                return -1;
-            }
-        }
-        return nums[nums.size()-1];
-    }
     vector<int> resultsArray(vector<int>& nums, int k) {
+        if (nums.empty() || k > nums.size()){
+            return {};
+        }
         int n=nums.size();
         vector<int>res(n-k+1);
+        //sliding window
         for(int i=0;i<n-k+1;i++){
-            vector<int>subvec(nums.begin()+i,nums.begin()+i+k);
-            res[i]=powerarr(subvec);
+            bool isconsecutive=true;
+            for(int j=i+1;j<i+k;j++){
+                if(nums[j]!=nums[j-1]+1){
+                    isconsecutive=false;
+                    break;
+                }
+            }
+            res[i]=isconsecutive?nums[i+k-1]:-1;
         }
         return res;
     }
