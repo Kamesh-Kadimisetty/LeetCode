6298e00e8d0da225fbe8532ce8e0ca56a00882ff
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-02 22:07:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (6.42%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    bool checkprefix(string word, string search){
-        int i=0;
-        int n=word.size(),m=search.size();
-        if(n<m) return false;
-        while(i<m){
-            if(word[i]==search[i]){
-                i++;
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
-    }
     int isPrefixOfWord(string sentence, string searchWord) {
-        int count=0;
-        vector<string>words;
-        int start=0;
-        for(int i=0;i<sentence.size();i++){
-            count++;
-            if(sentence[i]==' '){
-                words.push_back(sentence.substr(start,count));
-                start=i+1;
-                count=0;
+        istringstream sentenceStream(sentence);
+        string currentWord;
+        int wordPosition = 1;
+        while (sentenceStream >> currentWord) {
+            if (currentWord.length() >= searchWord.length() &&
+                currentWord.compare(0, searchWord.length(), searchWord) == 0) {
+                return wordPosition;
             }
-        }
-        words.push_back(sentence.substr(start,count));
-        for(int i=0;i<words.size();i++){
-            if(checkprefix(words[i],searchWord)){
-                return i+1;
-            }
+            wordPosition++;
         }
         return -1;
     }
