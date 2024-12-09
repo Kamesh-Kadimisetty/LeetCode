54dc9f235bacd617a8fa4bf1a991f28b20842d82
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-09 23:37:09 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (6.87%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         if(nums.size()<2) return true;
-        int flag=nums[0]%2;
         for(int i=1;i<nums.size();i++){
-            int rem=nums[i]%2;
-            if(rem==flag){
+            if(nums[i]%2==nums[i-1]%2){
                 return false;
             }
-            flag=rem;
         } 
         return true;
     }
