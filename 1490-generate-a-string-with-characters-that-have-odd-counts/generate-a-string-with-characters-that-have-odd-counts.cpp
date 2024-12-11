d75f285302afa1174fcf4de0da1cfe62d5854dfb
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-11 22:33:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (13.83%) - LeetSync

---
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string ans;
+        for(int i=0;i<n-1;i++){
+            ans+='a';
+        }
+        if(n%2==0){
+            ans+='b';
+        }
+        else{
+            ans+='a';
+        }
+        return ans;
+    }
+};
