da4e0e97996235fa7951113f98cabb02ba330b22
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-12 11:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (9.87%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count=1;
+        for(int i=1;i<word.size();i++){
+            if(word[i-1]==word[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
