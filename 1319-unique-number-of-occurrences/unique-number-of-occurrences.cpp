5797bce81d4f25cbc789c0de7e99770f518a9076
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-14 11:21:32 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (18.58%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        vector<int>ocuurences;
-        int cnt=1,start=arr[0];
-        for(int i=1;i<arr.size();i++){
-            if(arr[i]!=start){
-                ocuurences.push_back(cnt);
-                cnt=1;
-                start=arr[i];
-            }
-            else{
-                cnt++;
-            }
+        unordered_map<int,int>mpp;
+        for(int i=0;i<arr.size();i++){
+            mpp[arr[i]]++;
         }
-        ocuurences.push_back(cnt);
-        sort(ocuurences.begin(),ocuurences.end());
-        for(int i=1;i<ocuurences.size();i++){
-            if(ocuurences[i-1]==ocuurences[i]){
-                return false;
-            }
+        set<int>st;
+        for(auto it:mpp){
+            st.insert(it.second);
         }
-        return true;
+        return st.size()==mpp.size()?true:false;
     }
 };
