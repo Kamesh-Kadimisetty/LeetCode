b5075ddf4fc08ba374e64d97ae80198c32a46bad
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-16 09:59:02 +0530

    Time: 1 ms (40.45%) | Memory: 27.2 MB (38.94%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int findmin(vector<int>nums){
-        int ind=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<nums[ind]){
-                ind=i;
-            }
-        }
-        return ind;
-    }
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         vector<int>result(nums);
         while(k--){
-            int x=findmin(result);
-            result[x]=multiplier*result[x];
+            int ind=0;
+            for(int i=0;i<result.size();i++){
+                if(result[i]<result[ind]){
+                    ind=i;
+                }
+            }
+            result[ind]*=multiplier;
         }
         return result;
     }
