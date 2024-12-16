68d53f4de155f42745847caf857621ae19d65c3f
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-16 17:47:05 +0530

    Time: 3 ms (19.34%) | Memory: 23.9 MB (34.79%) - LeetSync

--- 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.cpp
+++ 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.cpp
@@ -3,12 +3,12 @@ public:
     int countCompleteDayPairs(vector<int>& hours) {
         int n=hours.size(),cnt=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if((hours[j]+hours[i])%24==0 && i!=j){
+            for(int j=i+1;j<n;j++){
+                if((hours[j]+hours[i])%24==0){
                     cnt++;
                 }
             }
         }
-        return cnt/2;
+        return cnt;
     }
 };
