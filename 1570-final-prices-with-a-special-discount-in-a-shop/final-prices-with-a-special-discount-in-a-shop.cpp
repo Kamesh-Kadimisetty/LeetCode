b2ade80e1ec9884a29b349653553b550e1354c91
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-18 07:09:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (24.39%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        int n=prices.size(),j;
-        vector<int>fprices;
+        int n=prices.size();
+        vector<int>result(prices);
         for(int i=0;i<n;i++){
-            int ind=-1;
             for(int j=i+1;j<n;j++){
                 if(prices[j]<=prices[i]){
-                    ind=j;
+                    result[i]-=prices[j];
                     break;
                 }
-                else{
-                    ind=-1;
-                }
-            }
-            if(ind==-1){
-                fprices.push_back(prices[i]);
-            }
-            else{
-                fprices.push_back(prices[i]-prices[ind]);
             }
         }
-        return fprices;
+        return result;
     }
 };
