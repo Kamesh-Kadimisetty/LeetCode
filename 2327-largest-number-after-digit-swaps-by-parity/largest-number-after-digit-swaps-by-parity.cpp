a4cf261cf1f36013adb7325f7941b3fcb0e0f160
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-19 22:41:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (47.87%) - LeetSync

--- 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.cpp
+++ 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     int largestInteger(int num) {
-        vector<int>odd;
-        vector<int>even;
         string s=to_string(num);
-        for(auto it:s){
-            if(it%2==0){
-                even.push_back(it);
-            }
-            else{
-                odd.push_back(it);
-            }
-        }
-        sort(even.rbegin(),even.rend());
-        sort(odd.rbegin(),odd.rend());
-        int k=0,j=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]%2==0){
-                s[i]=even[j];
-                j++;
-            }
-            else{
-                s[i]=odd[k];
-                k++;
+            for(int j=i+1;j<s.size();j++){
+                if(s[j]>s[i] && s[i]%2==s[j]%2){
+                    swap(s[i],s[j]);
+                    num=max(num,stoi(s));
+                }
             }
         }
-        return stoi(s);
+        return num;
     }
 };
