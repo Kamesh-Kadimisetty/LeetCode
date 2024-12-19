30eeda4b103bd510792a94945d0b9636e923d5e9
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-20 00:41:18 +0530

    Time: 5 ms (32.26%) | Memory: 23.3 MB (20.49%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,48 +1,19 @@
 class MinStack {
 public:
-    /** initialize your data structure here. */
-    stack<long> s;
-    long mn;
-    MinStack() {
-        
+    vector< pair<int,int> > s;
+    
+    MinStack() { }
+    
+    void push(int val) {
+        if(s.empty())
+            s.push_back({val,val});
+        else
+            s.push_back({val,min(s.back().second,val)});    
     }
     
-    void push(int x) {
-        if(s.empty()){
-            s.push(x);
-            mn = x;
-        }
-        else if(x <= mn){
-            // put 2*x - mn into stack
-            s.push(2ll*x - mn);
-            mn = x;
-        }
-        else{
-            s.push(x);
-        }
-    }
+    void pop() { s.pop_back(); }
     
-    void pop() {
-        if(s.empty()) return;
-        // stack top can never be smaller than mn, if it is then it indicates a flag which means before popping, you have to update mn
-        if(s.top() < mn){
-            // mn = 2*mn - s.top()
-            mn = 2ll*mn - s.top();
-        }
-        s.pop();
-    }
+    int top() { return s.back().first; }
     
-    // critical part
-    int top() {
-        if(s.empty()) return -1;
-        if(s.top() < mn){
-            return mn;
-        }
-        return s.top();
-    }
-    
-    int getMin() {
-        if(s.empty()) return -1;
-        return mn;
-    }
+    int getMin() { return s.back().second; }
 };
