06e5b53ffa41b6b232a13fcb622e17a4d47d9103
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2024-12-19 21:37:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (7.32%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -5,12 +5,10 @@ public:
         int chunks = 0, maxElement = 0;
         for (int i = 0; i < n; i++) {
             maxElement = max(maxElement, arr[i]);
-
             if (maxElement == i) {
                 chunks++;
             }
         }
-
         return chunks;
     }
 };
