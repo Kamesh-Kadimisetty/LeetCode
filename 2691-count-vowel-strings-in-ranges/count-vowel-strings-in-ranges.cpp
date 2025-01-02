db09f2f30691510c08f5cd1e1453efe96fca3630
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-01-02 20:59:15 +0530

    Time: 227 ms (5.16%) | Memory: 74.3 MB (28.83%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
+    bool isvowel(char c){
+        return c=='a' || c=='e' || c=='i' || c=='o' ||c=='u';
+    }
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
         int n=words.size();
-        int m=queries.size();
-        vector<int>ans;
-        vector<int>vowels(n);
-        int i=0;
-        for(auto it:words){
-            int s=it.size();
-            if((it[0]=='a' || it[0]=='e' || it[0]=='i' || it[0]=='o' || it[0]=='u')&& 
-                (it[s-1]=='a' || it[s-1]=='e' || it[s-1]=='i' || it[s-1]=='o' || it[s-1]=='u')){
-                    vowels[i]=1;
-            }
-            else{
-                vowels[i]=0;
-            }
-            i++;
-        }
         vector<int>prefixsum(n);
-        prefixsum[0]=vowels[0];
+        prefixsum[0]=isvowel(words[0].front())&&isvowel(words[0].back());
         cout<<prefixsum[0]<<endl;
         for(int i=1;i<n;i++){
-            prefixsum[i]=prefixsum[i-1]+vowels[i];
+            int add=0;
+            if(isvowel(words[i].front())&& isvowel(words[i].back())){
+                add=1;
+            }
+            prefixsum[i]=prefixsum[i-1]+add;
             cout<<prefixsum[i]<<endl;
         }
+        vector<int>ans;
         for(auto it:queries){
             int left=it[0];
             int right=it[1];
