d081a3e79116cfe13a4198d0781fc001d5ee2d8a
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-01-03 22:50:04 +0530

    Time: 6 ms (61.08%) | Memory: 12.3 MB (71.08%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -2,68 +2,23 @@ class Solution {
 public:
     int romanToInt(string s) {
         int n=s.size();
-        int i=n-1,result=0;
-        while(i>=0){
-            if(s[i]=='I'){
-                result+=1;
+        unordered_map<char,int>m;
+        m['I']=1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(m[s[i]]<m[s[i+1]]){
+                ans-=m[s[i]];
             }
-            else if(s[i]=='V'){
-                if(i>0 && s[i-1]=='I'){
-                    result+=4;
-                    i--;
-                }
-                else{
-                    result+=5;
-                }
+            else{
+                ans+=m[s[i]];
             }
-            else if(s[i]=='X'){
-                if(i>0 && s[i-1]=='I'){
-                    result+=9;
-                    i--;
-                }
-                else{
-                    result+=10;
-                }
-            }
-            else if(s[i]=='L'){
-                if(i>0 && s[i-1]=='X'){
-                    result+=40;
-                    i--;
-                }
-                else{
-                    result+=50;
-                }
-            }
-            else if(s[i]=='C'){
-                if(i>0 && s[i-1]=='X'){
-                    result+=90;
-                    i--;
-                }
-                else{
-                    result+=100;
-                }
-            }
-            else if(s[i]=='D'){
-                if(i>0 && s[i-1]=='C'){
-                    result+=400;
-                    i--;
-                }
-                else{
-                    result+=500;
-                }
-            }
-            else if(s[i]=='M'){
-                if(i>0 && s[i-1]=='C'){
-                    result+=900;
-                    i--;
-                }
-                else{
-                    result+=1000;
-                }
-            }
-            cout<<result<<endl;
-            i--;
         }
-        return result;
+        return ans;
     }
 };
