ca17fdaf665baefade25989c36fafc7c7c4f0b08
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-01-04 12:24:49 +0530

    Time: 368 ms (36.51%) | Memory: 16.3 MB (38.34%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int total=0,n=s.size();
-        set<char>letters;
-        for(auto it:s){
-            letters.insert(it);
+        int n=s.size(),total=0;
+        vector<int>first(26,-1);
+        vector<int>last(26,-1);
+        for(int i=0;i<n;i++){
+            if(first[s[i]-'a']==-1){
+                first[s[i]-'a']=i;
+            }
+            last[s[i]-'a']=i;
         }
-        for(char l:letters){
-            int i=-1;
-            int j=0;
-            for(int k=0;k<n;k++){
-                if(s[k]==l){
-                    if(i==-1){
-                        i=k;
-                    }
-                    j=k;
-                }
+        for(int i=0;i<26;i++){
+            if(first[i]==-1){
+                continue;
             }
             set<char>st;
-            for(int p=i+1;p<j;p++){
-                st.insert(s[p]);
+            for(int k=first[i]+1;k<last[i];k++){
+                st.insert(s[k]);
             }
             total+=st.size();
         }
