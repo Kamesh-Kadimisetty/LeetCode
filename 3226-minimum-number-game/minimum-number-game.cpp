896cc1044465b84f608d649955ec84f9c0460d8a
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-01-05 10:40:02 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (16.75%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> numberGame(vector<int>& nums) {
         vector<int>result;
         int n=nums.size();
-        for(int i=0;i<n;i+=2){
             sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i+=2){
             result.push_back(nums[i+1]);
             result.push_back(nums[i]);
         }
