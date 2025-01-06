c7847c70d11d8a1ef90b82ba34338c3f85d61e56
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-01-06 21:14:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (80.39%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n=boxes.size();
-        vector<int>ans;
+        vector<int>ans(n,0);
+        int leftballs=0,rightballs=0;
+        int leftmoves=0,rightmoves=0;
         for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(boxes[j]=='1'&&j!=i){
-                    count+=abs(j-i);
+            ans[i]+=leftmoves;
+            leftballs+=boxes[i]-'0';
+            leftmoves+=leftballs;
 
-                }
-            }
-            ans.push_back(count);
+            int j=n-i-1;
+            ans[j]+=rightmoves;
+            rightballs+=boxes[j]-'0';
+            rightmoves+=rightballs;
         }
         return ans;
     }
