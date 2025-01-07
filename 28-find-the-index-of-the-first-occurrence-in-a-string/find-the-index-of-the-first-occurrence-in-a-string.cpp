ac5bc4f10ec21557ed107e14e5701b257ed589ed
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-01-07 07:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (67.16%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(haystack.find(needle)!=string::npos){
-            return haystack.find(needle);
+        int n=haystack.size(),m=needle.size();
+        for(int i=0;i<=n-m;i++){
+            int j=0;
+            while(j<m){
+                if(haystack[i+j]!=needle[j]){
+                    break;
+                }
+                j++;
+            }
+            if(j==m){
+                return i;
+            }
         }
         return -1;
     }
