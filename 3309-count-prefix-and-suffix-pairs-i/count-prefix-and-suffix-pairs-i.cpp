98d62522d0019b848e9e375eb0f6f1269a8ce873
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-01-08 06:54:42 +0530

    Time: 11 ms (18.43%) | Memory: 23.5 MB (66.95%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
-    bool isPrefixAndSuffix(string s1,string s2){
-        int n=s1.size(),m=s2.size();
-        if(m<n) return false;
-        for(int i=0;i<n;i++){
-            if(s1[i]!=s2[i]){
-                return false;
-            }
-            int j=m-i-1,k=n-i-1;
-            if(s1[k]!=s2[j]){
-                return false;
-            }   
-        }
-        return true;
-        // string prefix=s2.substr(0,n);
-        // reverse(s2.begin(),s2.end());
-        // string suffix=s2.substr(0,n);
-        // reverse(suffix.begin(),suffix.end());
-        // return prefix==s1 && suffix==s1;
-    }
     int countPrefixSuffixPairs(vector<string>& words) {
         int n=words.size(),cnt=0;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(isPrefixAndSuffix(words[i],words[j])==true){
+                string s1=words[i];
+                string s2=words[j];
+                if(s1.size()>s2.size()) continue;
+                if(s2.find(s1)==0 && s2.rfind(s1)==s2.size()-s1.size()){
                     cnt++;
                 }
             }
