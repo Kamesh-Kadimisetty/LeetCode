1ac6fdb9a4467b0870572e1d6b5bacae40d33cf7
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-01-29 15:58:38 +0530

    Time: 1862 ms (5.13%) | Memory: 17.5 MB (16.78%) - LeetSync

---
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int result=0,n=s.size(),i=0;
+        stack<char>st;
+        while(i<n){
+            if(s[i]=='('){
+                st.push('(');
+            }
+            else{
+                if(i<n-1 && s[i+1]==')'){
+                    if(!st.empty()){
+                        st.pop();
+                        i++;
+                    }
+                    else{
+                        result+=1;
+                        i++;
+                    }
+                }  
+                else{
+                    if(!st.empty()){
+                        st.pop();
+                        result+=1;
+                    }
+                    else{
+                        result+=2;
+                    }
+                }
+            }
+            i++;
+            cout<<result<<endl;
+        }
+        return result+(2*st.size());
+    }
+};
