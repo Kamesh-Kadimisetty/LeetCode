bc515c42efc32c8afa01f3918dd637d21031a54d
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-02-07 00:47:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (48.60%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -17,28 +17,22 @@ public:
         int n=board.size();
         int m=board[0].size();
         vector<vector<int>>visited(n,vector<int>(m,0));
-        //Marking borders
         for(int i=0;i<n;i++){
-            //first column
             if(board[i][0]=='O' && !visited[i][0]){
                 dfs(i,0,board,visited);
             }
-            //Last column
             if(board[i][m-1]=='O' && !visited[i][m-1]){
                 dfs(i,m-1,board,visited);
             }
         }
         for(int i=0;i<m;i++){
-            //first row
             if(board[0][i]=='O' && !visited[0][i]){
                 dfs(0,i,board,visited);
             }
-            //Last row
             if(board[n-1][i]=='O' && !visited[n-1][i]){
                 dfs(n-1,i,board,visited);
             }
         }
-        //check if O with not connected with borders ans mark as x
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]=='O' && !visited[i][j]){
