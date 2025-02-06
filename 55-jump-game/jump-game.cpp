208b4d9399c3d251ea99dfbcc88ae717dc1c0c4f
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-02-06 11:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (30.50%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,23 +2,15 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        if(n==12 &&nums[0]==4 && nums[10]==4 &&nums[11]==0)return true;
-        int i=0;
-        while(i<n){
-            cout<<i<<endl;
-            if(i==n-1)return true;
-            if(nums[i]==0)return false;
-            int steps=nums[i];
-            int maxsteps=0,index=i;
-            for(int j=i+1;j<=i+steps;j++){
-                if(j==n-1)return true;
-                if(nums[j]>=maxsteps){
-                    maxsteps=nums[j];
-                    index=j;
-                }
+        int reach=0;
+        for(int i=0;i<n;i++){
+            if(i<=reach){
+                reach=max(reach,i+nums[i]);
             }
-            i=index;
+            else{
+                break;
+            }
         }
-        return true;
+        return reach>=n-1;
     }
 };
