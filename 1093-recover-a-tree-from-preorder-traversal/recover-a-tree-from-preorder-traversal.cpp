a6278c41ffa1d546e691c1f29ff74a2ac71996bb
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-02-22 14:50:43 +0530

    Time: 34 ms (9.63%) | Memory: 21.6 MB (64.33%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* recoverFromPreorder(string traversal) {
@@ -46,8 +35,6 @@ public:
                 }
             }
             st.push(root);
-            // cout<<st.size()<<" ";
-            // i++;
         }
         return dummy->left;
     }
