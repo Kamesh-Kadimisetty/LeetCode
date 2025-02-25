6149cda8d8706d993d4f3f220c5476aae1be2d45
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-02-26 00:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (15.60%) - LeetSync

--- 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.cpp
+++ 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.cpp
@@ -6,12 +6,10 @@ public:
         int val=0.05*n;
         double sum=accumulate(arr.begin(),arr.end(),0.0);
         double remove=0;
-        cout<<val<<endl;
         for(int i=0;i<val;i++){
             remove+=arr[i];
             remove+=arr[n-i-1];
         }
-        cout<<sum<<" "<<remove;
         return (sum-remove)/double(n-(2*val));
     }
 };
