4a851e5002e141e21612e3a1bd22b156f8144000
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-02-28 23:38:24 +0530

    Time: 13 ms (98.74%) | Memory: 13.3 MB (95.08%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string a, string b) {
+        int dp[1001][1001]={},m=a.size(),n=b.size();
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                dp[i+1][j+1]=a[i]==b[j]?dp[i][j]+1:max(dp[i][j+1],dp[i+1][j]);
+            }
+        }
+        string res;
+        while(m && n){
+            if (dp[m][n] == dp[m - 1][n]){
+                res += a[--m];
+            }
+            else if (dp[m][n] == dp[m][n - 1]){
+                res += b[--n]; 
+            }
+            else{
+                res += min(a[--m], b[--n]);
+            }
+        }
+        return a.substr(0, m) + b.substr(0, n) + string(rbegin(res), rend(res));
+    }
+};
