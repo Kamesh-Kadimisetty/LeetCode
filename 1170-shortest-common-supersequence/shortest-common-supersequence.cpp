e434941d9d1049e85b72014842d46f51cfa0df4b
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-02-28 23:39:28 +0530

    Time: 20 ms (93.24%) | Memory: 13.2 MB (95.63%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -9,16 +9,16 @@ public:
         }
         string res;
         while(m && n){
-            if (dp[m][n] == dp[m - 1][n]){
-                res += a[--m];
+            if (dp[m][n]==dp[m-1][n]){
+                res+=a[--m];
             }
-            else if (dp[m][n] == dp[m][n - 1]){
-                res += b[--n]; 
+            else if (dp[m][n]==dp[m][n - 1]){
+                res+=b[--n]; 
             }
             else{
-                res += min(a[--m], b[--n]);
+                res+=min(a[--m],b[--n]);
             }
         }
-        return a.substr(0, m) + b.substr(0, n) + string(rbegin(res), rend(res));
+        return a.substr(0,m)+ b.substr(0,n)+string(rbegin(res),rend(res));
     }
 };
