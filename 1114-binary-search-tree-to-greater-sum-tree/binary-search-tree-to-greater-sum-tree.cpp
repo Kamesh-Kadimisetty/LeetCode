329bffa8660ff3513dcdb82d454465885e4b1945
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-03-02 21:58:30 +0530

    Time: 3 ms (2.45%) | Memory: 11.4 MB (17.79%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int i=0;
+    void inorder(TreeNode* root,vector<int>&values){
+        if(root!=NULL){
+            inorder(root->left,values);
+            values.push_back(root->val);
+            inorder(root->right,values);
+        }
+    }
+    void changevalues(TreeNode* &root,vector<int>&values,int &totalsum){
+        if(root!=NULL){
+            changevalues(root->left,values,totalsum);
+            root->val=totalsum;
+            totalsum-=values[i];
+            i++;
+            changevalues(root->right,values,totalsum);
+        }
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        vector<int>values;
+        inorder(root,values);
+        int totalsum=accumulate(values.begin(),values.end(),0);
+        for(auto it:values){
+            cout<<it<<" ";
+        }
+        changevalues(root,values,totalsum);
+        return root;
+    }
+};
