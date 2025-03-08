900d3238a5b02a775c3ed5584d983653043868e7
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-03-08 08:00:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.01%) - LeetSync

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int n=blocks.size();
+        int black=0,mincolor=INT_MAX;
+        for(int i=0;i<k;i++){
+            if(blocks[i]=='B'){
+                black++;
+            }
+        }
+        mincolor=min(mincolor,k-black);
+        for(int i=1;i<=n-k;i++){
+            if(blocks[i+k-1]=='B'){
+                black++;
+            }
+            if(blocks[i-1]=='B'){
+                black--;
+            }
+            mincolor=min(mincolor,k-black);
+        }
+        return mincolor;
+    }
+};
