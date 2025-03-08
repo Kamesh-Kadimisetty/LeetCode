dd9e97343b327497dc8df0d399556c82b59dbad9
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-03-08 18:53:51 +0530

    Time: 14 ms (64.64%) | Memory: 17.1 MB (53.10%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>mat(n,vector<int>(n,0));
+        vector<vector<int>>mat(n,vector<int>(n,INT_MAX));
         for(auto it:edges){
             mat[it[0]][it[1]]=it[2];
             mat[it[1]][it[0]]=it[2];
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==0){
-                    mat[i][j]=1e9;
-                }
-                if(i==j){
-                    mat[i][j]=0;
-                }
-            }
+            mat[i][i]=0;
         }
         for(int via=0;via<n;via++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
+                    if(mat[i][via]==INT_MAX || mat[via][j]==INT_MAX){
+                        continue;
+                    }
                     mat[i][j]=min(mat[i][j],mat[i][via]+mat[via][j]);
                 }
             }
@@ -27,7 +23,7 @@ public:
         for(int i=0;i<n;i++){
             int cnt=0;
                 for(int j=0;j<n;j++){
-                    if(mat[i][j]<=distanceThreshold){
+                    if(mat[i][j]<=distanceThreshold && mat[i][j]!=0){
                         cnt++;
                     }
                 }
