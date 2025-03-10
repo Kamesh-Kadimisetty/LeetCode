79620373c615063008db8e63ca59528b3587e2f2
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-03-11 01:10:57 +0530

    Time: 22 ms (49.72%) | Memory: 46.6 MB (58.31%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    //USING DISJOINT SETS
+    vector<int> parent, size;
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size()<n-1)return -1;
+        cout<<connections.size();
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+        for(auto it:connections){
+            unionBySize(it[0],it[1]);//u v
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(findUPar(i)==i){
+                cnt++;
+            }
+        }
+        return cnt-1;
+    }
+};
