8cf5ce0442728d8fa5d5e081cb03a0830ab49a3d
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-03-13 23:19:43 +0530

    Time: 11 ms (77.74%) | Memory: 323 MB (85.62%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    bool ispossible(vector<int>& nums, vector<vector<int>>& queries,int mid){
-        int n=nums.size();
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size(),k=0,sum=0;
         vector<int>diff(n+1,0);
-        for(int i=0;i<mid;i++){
-            int l=queries[i][0],r=queries[i][1],k=queries[i][2];
-            diff[l]+=k;
-            diff[r+1]-=k;
-        }
-        int sum=0;
         for(int i=0;i<n;i++){
+            while(sum+diff[i]<nums[i]){
+                k++;
+                if(k>queries.size())return -1;
+                int l=queries[k-1][0],r=queries[k-1][1],val=queries[k-1][2];
+                if(r>=i){
+                    diff[max(i,l)]+=val;
+                    diff[r+1]-=val;
+                }
+            }
             sum+=diff[i];
-            if(sum<nums[i])return false;
         }
-        return true;
-    }
-    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries){
-        int n=nums.size();
-        int low=0,high=queries.size();
-        if(!ispossible(nums,queries,high)) return -1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(ispossible(nums,queries,mid)){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        return low;
+        return k;
     }
 };
