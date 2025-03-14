17e4b9ce67a9baa3979e5a13eaac7f5cc52addcc
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-03-14 21:35:56 +0530

    Time: 21 ms (56.38%) | Memory: 24.8 MB (29.00%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,21 +1,56 @@
+class Disjointset{
+public:
+    vector<int>parent,size;
+    Disjointset(int n){
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++) parent[i]=i;
+    }
+    int findparent(int node){
+        if(node==parent[node]){
+            return node;
+        }
+        return parent[node]=findparent(parent[node]);
+    }
+    void unionbysize(int u,int v){
+        int ulpu=findparent(u);
+        int ulpv=findparent(v);
+        if(ulpv==ulpu){
+            return;
+        }
+        if(size[ulpu]>size[ulpv]){
+            parent[ulpv]=ulpu;
+            size[ulpu]+=size[ulpv];
+        }
+        else{
+            parent[ulpu]=ulpv;
+            size[ulpv]+=size[ulpu];
+        }
+    }
+};
 class Solution {
 public:
-    int dfs(vector<vector<int>>&stones,int index,vector<bool>&visited,int&n){
-        visited[index]=true;
-        int result=0;
-        for(int i=0;i<n;i++)
-            if(!visited[i]&&(stones[i][0]==stones[index][0]||stones[i][1]==stones[index][1]))
-                result +=(dfs(stones,i,visited,n) + 1);
-        return result;
-    }
-    int removeStones(vector<vector<int>>&stones) {
-        int n = stones.size();
-        vector<bool>visited(n,0);
-        int result=0;
-        for(int i=0;i<n;i++){
-            if(visited[i]){continue;}
-            result+=dfs(stones,i,visited,n);
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        int maxrow=0;
+        int maxcol=0;
+        for(auto it:stones){
+            maxrow=max(maxrow,it[0]);
+            maxcol=max(maxcol,it[1]);
         }
-        return result;
+        Disjointset ds(maxrow+maxcol+1);
+        set<int>st;
+        for(auto it:stones){
+            int row=it[0];
+            int col=maxrow+1+it[1];
+            ds.unionbysize(row,col);
+            st.insert(row);
+            st.insert(col);
+        }
+        int cnt=0;
+        for(auto it:st){
+            if(ds.findparent(it)==it)cnt++;
+        }
+        return n-cnt;
     }
 };
