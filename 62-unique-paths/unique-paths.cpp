dbdd3a4a74c51a85614a74dbd49cce1738976f0a
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-03-22 23:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.17%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int func(int i,int j,int m,int n,vector<vector<int>>&dp){
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(i>m-1 || j>n-1){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int right=0,down=0;
+        if(i<m-1){
+            right=func(i+1,j,m,n,dp);
+        }
+        if(j<n-1){
+            down=func(i,j+1,m,n,dp);
+        }
+        return dp[i][j]=right+down;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return func(0,0,m,n,dp);
+    }
+};
