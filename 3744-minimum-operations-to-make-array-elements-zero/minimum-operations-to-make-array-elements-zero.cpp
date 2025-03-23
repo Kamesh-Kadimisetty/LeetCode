f8260ae5d481fd96de99e90214683a1e845eb997
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-03-23 11:25:33 +0530

    Time: 123 ms (18.69%) | Memory: 207 MB (33.25%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -3,10 +3,10 @@ public:
     long long minOperations(vector<vector<int>>& queries) {
         long long result=0;
         for(auto it:queries){
-            long long l=it[0],h=it[1],cnt=0;
+            long long low=it[0],high=it[1],cnt=0;
             for(long long d=1,prev=1;d<17;d++){
                 long long curr=prev*4LL;
-                long long r=max(l,prev),k=min(h,curr-1);
+                long long r=max(low,prev),k=min(high,curr-1);
                 if(r<=k)cnt+=(k-r+1)*d;
                 prev=curr;
             }
