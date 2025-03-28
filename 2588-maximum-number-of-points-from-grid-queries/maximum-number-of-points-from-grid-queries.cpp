ad8d3954660a258fa94a9825ff53c71bd5bd5e44
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-03-29 00:31:50 +0530

    Time: 87 ms (85.14%) | Memory: 41.9 MB (65.94%) - LeetSync

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    bool valid(int i, int j, int n, int m) {
-        return i >= 0 && j >= 0 && i < n && j < m;
-    }
-    vector<int> maxPoints(vector<vector<int>>& nums, vector<int>& queries) {
-        int n = nums.size(), m = nums[0].size();
-        vector<int> res(queries.size());
-        vector<pair<int, int>> q(queries.size());
-        for (int i = 0; i < queries.size(); i++) q[i] = {queries[i], i};
-        sort(q.begin(), q.end());
-        vector<vector<int>> visited(n, vector<int>(m, -1));
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>> minh;
-        minh.push({nums[0][0], {0, 0}});
-        visited[0][0] = 1;
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
-        int count = 0;
-        for (int i = 0; i < queries.size(); i++) {
-            while (!minh.empty() && minh.top().first < q[i].first) {
-                count++;
-                int x = minh.top().second.first;
-                int y = minh.top().second.second;
-                minh.pop();
-                for (int j = 0; j < 4; j++) {
-                    int newx = x + dx[j];
-                    int newy = y + dy[j];
-                    if (valid(newx, newy, n, m) && visited[newx][newy] == -1) {
-                        visited[newx][newy] = 1;
-                        minh.push({nums[newx][newy], {newx, newy}});
+    vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int k=queries.size();
+        vector<int>result(k,0);
+        vector<pair<int,int>>sortedqueries;
+        for(int i=0;i<k;i++){
+            sortedqueries.push_back({queries[i],i});
+        }
+        sort(sortedqueries.begin(),sortedqueries.end());
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,
+                    greater<pair<int,pair<int,int>>>>pq;  //{val,{row,col}}
+        pq.push({grid[0][0],{0,0}});
+        visited[0][0]=1;
+        int total=0;
+        vector<pair<int,int>>directions={{0,1},{0,-1},{1,0},{-1,0}};
+        for(auto it:sortedqueries){
+            int query=it.first;
+            int index=it.second;
+            while(!pq.empty() && pq.top().first<query){
+                int val=pq.top().first;
+                int row=pq.top().second.first;
+                int col=pq.top().second.second;
+                pq.pop();
+                total++;
+                for(auto [dr,dc]:directions){
+                    int nr=row+dr;
+                    int nc=col+dc;
+                    if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc]){
+                        pq.push({grid[nr][nc],{nr,nc}});
+                        visited[nr][nc]=1;
                     }
                 }
             }
-            res[q[i].second] = count;
+            result[index]=total;
         }
-        return res;
+        return result;
     }
 };
