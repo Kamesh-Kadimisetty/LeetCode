a9a7d0ed84e0476491dcf2ccf1b99b1dd0dd33cb
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-04-19 22:14:03 +0530

    Time: 3 ms (88.27%) | Memory: 14.8 MB (61.00%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    int totalways=0;
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        calculateways(nums,0,0,target);
-        return totalways;
-    }
-    void calculateways(vector<int>&nums,int ind,int currsum,int target){
-        int n=nums.size();
-        if(ind==n){
-            if(currsum==target){
-                totalways++;
+    int findWays(vector<int> &num, int tar){
+        int n = num.size();
+        vector<int> prev(tar+1,0);
+        if(num[0] == 0) prev[0] =2;  
+        else prev[0] = 1;  
+        
+        if(num[0]!=0 && num[0]<=tar) prev[num[0]] = 1;  
+        for(int ind = 1; ind<n; ind++){
+            vector<int> cur(tar+1,0);
+            for(int target= 0; target<=tar; target++){
+                int notTaken = prev[target];
+        
+                int taken = 0;
+                    if(num[ind]<=target)
+                        taken = prev[target-num[ind]];
+            
+                cur[target]= (notTaken + taken);
             }
+            prev = cur;
         }
-        else{
-            calculateways(nums,ind+1,currsum+nums[ind],target);
-            calculateways(nums,ind+1,currsum-nums[ind],target);
+        return prev[tar];
+    }
+
+    int countPartitions(int d, vector<int>& arr){
+        int totSum = 0,n=arr.size();
+        for(int i=0; i<n;i++){
+            totSum += arr[i];
         }
+        if(totSum-d <0 || (totSum-d)%2 ) return 0;
+        return findWays(arr,(totSum-d)/2);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return countPartitions(target,nums);
     }
 };
