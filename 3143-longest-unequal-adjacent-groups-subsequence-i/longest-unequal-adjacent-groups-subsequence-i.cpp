b16e2c14ce2c70b7dfa1cd08bc38852a7256fb06
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-05-15 10:31:43 +0530

    Time: 0 ms (100.00%) | Memory: 30 MB (71.98%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
-    // vector<string>ans;
-    // void func(int ind,vector<string>& words,vector<int>& groups,vector<string>& result){
-    //     if(ind>=words.size()){
-    //         if(res.size()>ans.size()){
-    //             ans=res;
-    //         }
-    //         return ;
-    //     }
-
-    // }
     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
         vector<string>result;
-        // func(0,words,groups,result);
         int n=words.size(),rem=groups[0];
         result.push_back(words[0]);
         for(int i=1;i<n;i++){
