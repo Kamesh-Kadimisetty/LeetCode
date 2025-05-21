0f1e57cc643028e360d9c06849b00271c747d9b8
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-05-21 18:10:58 +0530

    Time: 32 ms (55.64%) | Memory: 15 MB (47.79%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    bool func(int i,int j,string &s,string &p,vector<vector<int>>& dp){
-        if(i==0 && j==0) return true;
-        if(i==0){
-            for(int k=1;k<=j;k++)
-                if(p[k-1]!='*') return false;
-            return true;
-        }
-        if(j==0 || i==0) return false;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i-1]==p[j-1] || p[j-1]=='?'){
-            if(func(i-1,j-1,s,p,dp)==true) return dp[i][j]=true;
-        }
-        if(p[j-1]=='*'){
-            if((func(i-1,j,s,p,dp) || func(i,j-1,s,p,dp)) ==true) return dp[i][j]=true; 
-        }
-        return dp[i][j]=false;
-    }
     bool isMatch(string s, string p) {
         int n=s.size(),m=p.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return func(n,m,s,p,dp);
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        dp[0][0]=true;
+        for(int j = 1; j <= m; j++) {
+            if (p[j - 1] == '*')
+                dp[0][j] = dp[0][j - 1];
+            else
+                break;
+        }
+
+        //DP TABLE
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j]=dp[i-1][j] || dp[i][j-1]; 
+                }
+                else dp[i][j]=false;
+            }
+        }
+        return dp[n][m];
     }
 };
