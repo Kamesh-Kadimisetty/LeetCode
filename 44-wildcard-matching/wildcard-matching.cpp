740aaf4ae4106b3d20570aeb9f90ce30c11f06a1
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-05-21 18:15:32 +0530

    Time: 55 ms (8.68%) | Memory: 14.9 MB (51.49%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -4,13 +4,12 @@ public:
         int n=s.size(),m=p.size();
         vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
         dp[0][0]=true;
-        for(int j = 1; j <= m; j++) {
-            if (p[j - 1] == '*')
-                dp[0][j] = dp[0][j - 1];
+        for(int j=1;j<=m;j++){
+            if (p[j-1]=='*')
+                dp[0][j]=dp[0][j - 1];
             else
                 break;
         }
-
         //DP TABLE
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
