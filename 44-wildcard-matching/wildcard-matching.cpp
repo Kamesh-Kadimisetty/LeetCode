ee078e534703134a3790b07642ffbfb0f229ef89
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-05-21 17:57:21 +0530

    Time: 45 ms (11.59%) | Memory: 32 MB (5.84%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     bool func(int i,int j,string &s,string &p,vector<vector<int>>& dp){
-        if(i<0 && j<0) return true;
-        if(i<0){
-            for(int k=0;k<=j;k++)
-                if(p[k]!='*') return false;
+        if(i==0 && j==0) return true;
+        if(i==0){
+            for(int k=1;k<=j;k++)
+                if(p[k-1]!='*') return false;
             return true;
         }
-        if(j<0 || i<0) return false;
+        if(j==0 || i==0) return false;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j] || p[j]=='?'){
+        if(s[i-1]==p[j-1] || p[j-1]=='?'){
             if(func(i-1,j-1,s,p,dp)==true) return dp[i][j]=true;
         }
-        if(p[j]=='*'){
+        if(p[j-1]=='*'){
             if((func(i-1,j,s,p,dp) || func(i,j-1,s,p,dp)) ==true) return dp[i][j]=true; 
         }
         return dp[i][j]=false;
     }
     bool isMatch(string s, string p) {
         int n=s.size(),m=p.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return func(n-1,m-1,s,p,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return func(n,m,s,p,dp);
     }
 };
