14f7ddd569a0ca0da1cdd0c11ff6a9aa260badad
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-05-22 19:33:34 +0530

    Time: 392 ms (45.69%) | Memory: 211.3 MB (46.16%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,31 +1,8 @@
-// class Solution {
-// public:
-//     int func(int ind,int buy,vector<int>& prices,vector<vector<vector<int>>>&dp,int chns){
-//         int n=prices.size();
-//         if(ind==n || chns==0){
-//             return 0;
-//         }
-//         if(dp[ind][buy][chns]!=-1) return dp[ind][buy][chns];
-//         int profit=0;
-//         if(buy){
-//             profit=max(-prices[ind]+func(ind+1,0,prices,dp,chns),func(ind+1,1,prices,dp,chns));
-//         }
-//         else{
-//             profit=max(prices[ind]+func(ind+1,1,prices,dp,chns-1),func(ind+1,0,prices,dp,chns));
-//         }
-//         return dp[ind][buy][chns]=profit;
-//     }
-//     int maxProfit(vector<int>& prices) {
-//         int n=prices.size();
-//         vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-//         return func(0,1,prices,dp,2);
-//     }
-// };
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(4,0)));
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 for(int chns=1;chns<3;chns++){
