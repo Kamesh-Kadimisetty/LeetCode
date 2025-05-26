e84fed3d1f324dbf7fb490996f77601d74ec7f2a
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-05-26 21:56:01 +0530

    Time: 355 ms (79.50%) | Memory: 191.1 MB (30.26%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int dfs(int node,string &colors,vector<vector<int>>&adj,vector<int>&visited,vector<vector<int>>&count){
-        if(!visited[node]){
-            visited[node]=1;
-            for(auto it:adj[node]){
-                if(dfs(it,colors,adj,visited,count)==INT_MAX) return INT_MAX;
-                for(int c=0;c<26;c++){
-                    count[node][c]=max(count[node][c],count[it][c]);
-                }
+        if(visited[node]==1) return INT_MAX;
+        if(visited[node]==2) return count[node][colors[node]-'a'];
+        visited[node]=1;
+        for(auto it:adj[node]){
+           if(dfs(it,colors,adj,visited,count)==INT_MAX) return INT_MAX;
+            for(int c=0;c<26;c++){
+                count[node][c]=max(count[node][c],count[it][c]);
             }
-            count[node][colors[node]-'a']++;
-            visited[node]=2;
         }
-        return visited[node]==2?count[node][colors[node]-'a']:INT_MAX;
+        count[node][colors[node]-'a']++;
+        visited[node]=2;
+        return count[node][colors[node]-'a'];
     }
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         int n=colors.size();
