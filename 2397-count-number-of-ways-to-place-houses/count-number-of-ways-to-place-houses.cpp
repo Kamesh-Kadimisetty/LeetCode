863c6d8da7afdb466178dcaa5e35837fb07767c8
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-05-27 13:44:08 +0530

    Time: 3 ms (78.14%) | Memory: 12.5 MB (56.01%) - LeetSync

--- 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.cpp
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int MOD=1e9+7;
-    int func(int ind,int canplace,int plots,vector<vector<int>>&dp){
-        if(ind==plots) return 1;
-        if(dp[ind][canplace]!=-1) return dp[ind][canplace];
-        int put=0,notput=0;
-        if(canplace==1){
-            put=func(ind+1,0,plots,dp);
-            notput=func(ind+1,1,plots,dp);
-        }
-        else{
-            notput=func(ind+1,1,plots,dp);
-        }
-        return dp[ind][canplace]=(put+notput)%MOD;
-    }
     int countHousePlacements(int n) {
         if(n==1) return 4;
         if(n==2) return 9;
