a8dcf4fecdc88439bf6bab8a5b2ce41815de7464
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-05-28 21:57:24 +0530

    Time: 7 ms (22.17%) | Memory: 10.4 MB (64.71%) - LeetSync

--- 2786-find-the-longest-semi-repetitive-substring/find-the-longest-semi-repetitive-substring.cpp
+++ 2786-find-the-longest-semi-repetitive-substring/find-the-longest-semi-repetitive-substring.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int longestSemiRepetitiveSubstring(string s) {
-        int n=s.size(),maxlen=0,prev=0;
+        int n=s.size(),maxlen=0,i=0,j=1,last=0;
         if(n==1) return 1;
-        for(int i=0;i<n;i++){
-            int cnt=0;
-            for(int j=i+1;j<n;j++){
-                if(s[j]==s[j-1]) cnt++;
-                if(cnt==2) break;
-                maxlen=max(maxlen,j-i+1);
+        while(j<n){
+            if(s[j-1]==s[j]){
+                i=last;
+                last=j;
             }
+            maxlen=max(maxlen,j-i+1);
+            j++;
         }
         return maxlen;
     }
