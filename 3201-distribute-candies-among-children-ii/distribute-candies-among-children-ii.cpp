a2ee8c21239172b27eeb07cbfa5b33976e821814
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-06-01 21:33:20 +0530

    Time: 36 ms (12.35%) | Memory: 8.9 MB (57.06%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -3,8 +3,9 @@ public:
     long long distributeCandies(int n, int limit) {
         long long ans=0;
         for(int i=0;i<=min(limit,n);i++){
-            if(n-i>2*limit) continue;
-            ans+=min(n-i,limit)-max(0,n-i-limit)+1;
+            if(n-i<=2*limit){
+                ans+=min(n-i,limit)-max(0,n-i-limit)+1;
+            }
         }
         return ans;
     }
