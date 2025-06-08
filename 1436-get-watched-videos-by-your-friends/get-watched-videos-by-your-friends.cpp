8c78d7c7e77409546cbda86d1eaf6cfb9d4313d5
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-06-09 00:41:06 +0530

    Time: 51 ms (16.31%) | Memory: 48.1 MB (13.85%) - LeetSync

---
+++ 1436-get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> watchedVideosByFriends(vector<vector<string>>& watchedVideos, vector<vector<int>>& friends, int id, int level) {
+        int n=watchedVideos.size();
+        vector<vector<int>>adj(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<friends[i].size();j++){
+                adj[i].push_back(friends[i][j]);
+            }
+        }
+        vector<int>visited(n,0);
+        queue<pair<int,int>>qu;
+        qu.push({id,0});
+        visited[id]=1;
+        unordered_map<string,int>mpp;
+        while(!qu.empty()){
+            int node=qu.front().first;
+            int lvl=qu.front().second;
+            qu.pop();
+            if(lvl==level){
+                for(auto it:watchedVideos[node]) mpp[it]++;
+            }
+            for(auto it:adj[node]){
+                if(!visited[it] && lvl+1<=level){
+                    qu.push({it,lvl+1});
+                    visited[it]=1;
+                } 
+            }
+        }
+        priority_queue<pair<int,string>,vector<pair<int,string>>,
+                            greater<pair<int,string>>>pq;
+        for(auto it:mpp){
+            pq.push({it.second,it.first});
+        }
+        vector<string>result;
+        while(!pq.empty()){
+            result.push_back(pq.top().second);
+            pq.pop();
+        }
+        return result;
+    }
+};
