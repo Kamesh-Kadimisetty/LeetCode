74ee48cfc3aee75790dd4edbef28382248af5e22
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-06-09 01:15:47 +0530

    Time: 55 ms (20.58%) | Memory: 105 MB (8.05%) - LeetSync

--- 3151-minimum-processing-time/minimum-processing-time.cpp
+++ 3151-minimum-processing-time/minimum-processing-time.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int minProcessingTime(vector<int>& processorTime, vector<int>& tasks) {
         int n=tasks.size();
-        sort(tasks.begin(),tasks.end());
-        sort(processorTime.rbegin(),processorTime.rend());
+        sort(tasks.rbegin(),tasks.rend());
+        sort(processorTime.begin(),processorTime.end());
         int result=INT_MIN;
-        for(int i=0;i<n;i+=4){
-            int x=processorTime[i/4];
-            for(int j=i;j<i+4;j++){
-                result=max(result,x+tasks[j]);
-            }
+        for(int i=0,j=0;i<n;i+=4){
+            result=max(result,processorTime[j++]+tasks[i]);
         }
         return result;
     }
