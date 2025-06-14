8dc379ca9291dfc104d52177a9dd60caa33b403d
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-06-14 10:46:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (18.49%) - LeetSync

---
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string maxnum=to_string(num);
+        char mapdig;
+        for(int i=0;i<maxnum.size();i++){
+            if(maxnum[i]!='9'){
+                mapdig=maxnum[i];
+                maxnum[i]='9';
+                break;
+            }
+        }
+        for(int i=0;i<maxnum.size();i++){
+            if(maxnum[i]==mapdig) maxnum[i]='9';
+        }
+        string minnum=to_string(num);
+        mapdig=minnum[0];
+        for(int i=0;i<minnum.size();i++){
+            if(minnum[i]==mapdig) minnum[i]='0';
+        }
+        return stoi(maxnum)-stoi(minnum);
+    }
+};
