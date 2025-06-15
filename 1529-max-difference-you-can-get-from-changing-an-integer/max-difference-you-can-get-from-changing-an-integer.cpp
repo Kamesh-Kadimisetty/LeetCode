80de4c09ba2ddc2f3bc759b2c9c81427e8ac0b10
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-06-15 16:35:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (43.13%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maxDiff(int num) {
-        string maxnum=to_string(num);
-        char mapchar;
+        string maxnum=to_string(num),minnum=to_string(num);
+        char mapchar,ch='x';
         int n=maxnum.size();
         for(int i=0;i<n;i++){
             if(maxnum[i]!='9'){
@@ -13,8 +13,6 @@ public:
         for(int i=0;i<n;i++){
             if(maxnum[i]==mapchar) maxnum[i]='9';
         }
-        string minnum=to_string(num);
-        char ch='x';
         for(int i=0;i<n;i++){
             if(minnum[i]!='1' && minnum[i]!='0'){
                 mapchar=minnum[i];
@@ -27,7 +25,6 @@ public:
                 if(minnum[i]==mapchar) minnum[i]=ch;
             }
         }
-        cout<< (maxnum)<<" "<<(minnum);
         return stoi(maxnum)-stoi(minnum);
     }
 };
