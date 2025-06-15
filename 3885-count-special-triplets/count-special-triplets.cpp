03993129aefe602277919d4388d3127d9991561c
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-06-15 20:52:25 +0530

    Time: 945 ms (29.44%) | Memory: 428 MB (54.55%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -2,22 +2,13 @@ class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
         unordered_map<int,int>mpp1,mpp2;
-        int n=nums.size(),zeros=0,cnt=0,MOD=1e9+7;
+        int cnt=0,MOD=1e9+7;
         for(auto it:nums) mpp1[it]++;
         for(auto it:nums){
-            if(it==0){
-                zeros++;
-                continue;
-            }
-            int x=2*it;
-            if(mpp1.find(x)!=mpp1.end() && mpp2.find(x)!=mpp2.end()){
-                cnt=(cnt+(mpp1[x]*mpp2[x])%MOD)%MOD;
-            }
             mpp1[it]--;
-            if(mpp1[it]==0) mpp1.erase(it);
+            cnt=(cnt+1LL*mpp1[2*it]*mpp2[2*it])%MOD;
             mpp2[it]++;
         }
-        cnt=cnt+(((1LL*zeros*(zeros-1))*(zeros-2))/6)%MOD;
         return cnt;
     }
 };
