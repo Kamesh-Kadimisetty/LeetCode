b3bb387096346ee2fe7c7d66a8111bf6b27263c6
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-06-17 23:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (25.90%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int count=0;
-        while(start>0 || goal >0){
-            if((start &1)!=(goal&1)){
-                count++;
-            }
-            start>>=1;
-            goal>>=1;
+        int ans=start^goal,cnt=0;
+        for(int i=0;i<32;i++){
+            if(ans&(1<<i)) cnt++;
         }
-        return count;
+        return cnt;
     }
 };
