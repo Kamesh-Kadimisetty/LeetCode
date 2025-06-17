ca5c9cdbb5a5739d4a11bff81980f84ede35df32
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-06-17 16:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.26%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<1)return 0;
-       while(n%2==0){
-            n=n/2;
-       }
-       return n==1;
+        if(n<=0) return false;
+        return!(n&(n-1));
     }
 };
