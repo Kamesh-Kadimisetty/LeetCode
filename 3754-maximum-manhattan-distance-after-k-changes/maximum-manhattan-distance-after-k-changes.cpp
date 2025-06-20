07a2dff9cca7971482e395280218ea0e84aacfdb
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-06-20 13:42:20 +0530

    Time: 248 ms (18.49%) | Memory: 38.7 MB (30.00%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxDistance(string s, int k) {
-        int n=s.size(),maxdist=INT_MIN,i=0;
+        int n=s.size(),maxdist=INT_MIN;
         unordered_map<char,int>mpp;
-        for(auto it:s){
-            i++;
-            mpp[it]++;
+        for(int i=0;i<n;i++){
+            mpp[s[i]]++;
             int dist=abs(mpp['N']-mpp['S'])+abs(mpp['E']-mpp['W']);
-            maxdist=max(maxdist,dist+min(2*k,i-dist));
+            maxdist=max(maxdist,dist+min(2*k,i+1-dist));
         } 
         return maxdist;
     }
