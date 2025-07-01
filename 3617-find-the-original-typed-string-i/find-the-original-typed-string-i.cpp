c9d1c4571ef202f725012243f5a2514ba06cf286
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-07-01 06:58:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (29.69%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        int count=1;
-        for(int i=1;i<word.size();i++){
-            if(word[i-1]==word[i]){
-                count++;
+        int n=word.size(),result=1,cnt=1;
+        for(int i=1;i<n;i++){
+            if(word[i]==word[i-1]){
+                cnt++;
             }
+            else{
+                result+=(cnt-1);
+                cnt=1;
+            }
+            if(i==n-1) result+=(cnt-1);
         }
-        return count;
+        return result;
     }
 };
