8b77bdedd80157bc39c9b8619ad453374e557a23
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-07-14 22:52:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (50.47%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* prev=NULL;
+    void flatten(TreeNode* root) {
+        if(root==NULL){
+            return;
+        }
+        flatten(root->right);
+        flatten(root->left);
+        root->left=NULL;
+        root->right=prev;
+        prev=root;
+        return;
+    }
+};
