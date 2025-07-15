321311898a7b4affe514f81e129092d7691bcc38
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-07-15 09:51:12 +0530

    Time: 0 ms (100.00%) | Memory: 59.2 MB (81.19%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -1,41 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if (root == nullptr) {
-            return new TreeNode(val);
-        }
+        TreeNode* newnode=new TreeNode(val);
+        if(root==NULL) return newnode;
         TreeNode* temp=root;
-        while(true){
-            if(temp->val<val){
-                if(temp->right==NULL){
-                    temp->right=new TreeNode(val);
-                    break;
-                }
-                else{
-                temp=temp->right;
-                }
+        while(temp!=NULL){
+            if(temp->val>val){
+                if(temp->left==NULL) break;
+                temp=temp->left;
             }
             else{
-                if(temp->left==NULL){
-                    temp->left=new TreeNode(val);
-                    break;
-                }
-                else{
-                temp=temp->left;
-                }
+                if(temp->right==NULL) break;
+                temp=temp->right;
             }
+
         }
+        if(temp->val>val) temp->left=newnode;
+        else temp->right=newnode;
         return root;
     }
 };
