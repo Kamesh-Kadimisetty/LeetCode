dc5151339a642fe8a1751d856f4f6d24caf518cd
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-07-15 08:15:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (33.01%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -1,8 +1,5 @@
 class Solution {
 public:
-    bool iscorrect(char ch){
-        return (ch>='0' && ch<='9') || (ch>='a' && ch<='z') || (ch>='A' && ch<='Z');
-    }
     bool isvowel(char ch){
         return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
     }
@@ -11,8 +8,8 @@ public:
         if(n<3) return false;
         int flag1=0,flag2=0;
         for(auto it:word){
-            if(!iscorrect(it)) return false;
-            if((it>='a' && it<='z') || (it>='A' && it<='Z')){
+            if(!isalnum(it)) return false;
+            if(isalpha(it)){
                 if(isvowel(tolower(it))) flag1=1;
                 else flag2=1;
             }
