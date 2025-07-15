af753448805c18fec09fd0f8ddf1b078a03e6e5a
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-07-15 12:35:02 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (18.31%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,26 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool inorder(TreeNode* root,long long& prev){
         if(root!=NULL){
-            if(inorder(root->left,prev)==false) return false;
-            if(root->val<=prev){
+            if(inorder(root->left,prev)==false){
                 return false;
             }
-            else{
-                prev=root->val;
+            if(root->val<=prev) return false;
+            prev=root->val;
+            if(inorder(root->right,prev)==false){
+                return false;
             }
-            if(inorder(root->right,prev)==false) return false;
         }
         return true;
     }
