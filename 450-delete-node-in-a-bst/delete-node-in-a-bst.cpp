6418bd0d9fd6872606dbcb053e4843b0ceddf2e2
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-07-15 10:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 34.2 MB (63.49%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    TreeNode* helper(TreeNode* root){
-        if(root->left==NULL){
-            return root->right;
-        }
-        else if(root->right==NULL){
-            return root->left;
-        }
-        TreeNode* rightchild=root->right;
-        TreeNode* lastright=findlastright(root->left);
-        lastright->right=rightchild;
-        return root->left;
-    }
     TreeNode* findlastright(TreeNode* root){
         while(root->right!=NULL){
             root=root->right;
         }
         return root;
     }
+    TreeNode* helper(TreeNode* root){
+        if(root->left==NULL){
+            return root->right;
+        }
+        if(root->right==NULL){
+            return root->left;
+        }
+        TreeNode* temp=findlastright(root->left);
+        temp->right=root->right;
+        return root->left;
+    }
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root==NULL) return root;
         if(root->val==key){
             return helper(root);
         }
-        TreeNode* curr=root;
+        TreeNode* dummy=root;
         while(root!=NULL){
             if(root->val>key){
                 if(root->left!=NULL && root->left->val==key){
                     root->left=helper(root->left);
+                    break;
                 }
                 else{
                     root=root->left;
@@ -36,12 +36,13 @@ public:
             else{
                 if(root->right!=NULL && root->right->val==key){
                     root->right=helper(root->right);
+                    break;
                 }
                 else{
                     root=root->right;
                 }
             }
         }
-        return curr;
+        return dummy;
     }
 };
