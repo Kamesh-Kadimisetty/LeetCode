28ccbfaf1401b96192663dbbb4cf238ccd9b6d2c
Kamesh Kadimisetty <323103311022@gvpce.ac.in>
2025-07-31 01:28:50 +0530

    Time: 650 ms (73.04%) | Memory: 274.5 MB (70.98%) - LeetSync

--- 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -35,7 +35,7 @@ public:
                 qu.push(ind-1);
                 distance[ind-1]=distance[ind]+1;
             }
-            if(primes[nums[ind]]==false || st.find(nums[ind]) != st.end()
+            if(primes[nums[ind]]==false || st.find(nums[ind])!=st.end()
 ) continue;
             for(int x=nums[ind];x<=maxi;x+=nums[ind]){
                 if(mpp.find(x)==mpp.end()) continue;
